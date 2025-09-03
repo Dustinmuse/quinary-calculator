c96308a67a6b7986bb32b9e1b948f80560d3db63
Dustin Muse <122392558+Dustinmuse@users.noreply.github.com>
2025-09-03 23:16:07 +0000

    Fixed string to int causing test failure

--- tests/test_basic_operations.py
+++ tests/test_basic_operations.py
@@ -4,7 +4,6 @@ from basic_operations import add, subtract, multiply, divide
 class TestOperations(unittest.TestCase):
 
     def test_add(self):
-<<<<<<< Updated upstream
         self.assertEqual(add(2, 2), 4)
 
     def test_substract(self):
@@ -18,21 +17,6 @@ class TestOperations(unittest.TestCase):
 
     def test_divide_by_zero(self):
         self.assertEqual(divide(4, 0), "Error: Division by zero")
-=======
-        self.assertEqual(add("2", "2"), "4")
-
-    def test_substract(self):
-        self.assertEqual(subtract("4", "3"), "1")
-    
-    def test_multiply(self):
-        self.assertEqual(multiply("3", "2"), "11")
-
-    def test_divide(self):
-        self.assertEqual(divide("4", "2"), "2")
-
-    def test_divide_by_zero(self):
-        self.assertEqual(divide("4", "0"), "Error: Division by zero")
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
     unittest.main()
