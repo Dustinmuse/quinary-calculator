29cd959d34eee2964f612921904f71be9665ecfb
Dustin Muse <Dustin.muse12@gmail.com>
2025-09-03 10:38:56 -0400

    Fixed error with function calls

--- tests/test_quinary_logic.py
+++ tests/test_quinary_logic.py
@@ -1,19 +1,21 @@
 import unittest
-from quinary_logic import decimal_to_quinary, quinary_to_decimal
+from quinary_logic import QuinaryCalculator
 
 class TestOperations(unittest.TestCase):
-    
+    def setUp(self):
+        self.calc = QuinaryCalculator()
+
     def test_decimal_to_quinary(self):
-        self.assertEqual(decimal_to_quinary(10), 20)
-        self.assertEqual(decimal_to_quinary(0), 0)
-        self.assertEqual(decimal_to_quinary(4), 4)
-        self.assertEqual(decimal_to_quinary(25), 100)
+        self.assertEqual(self.calc.decimal_to_quinary(10), 20)
+        self.assertEqual(self.calc.decimal_to_quinary(0), 0)
+        self.assertEqual(self.calc.decimal_to_quinary(4), 4)
+        self.assertEqual(self.calc.decimal_to_quinary(25), 100)
 
     def test_quinary_to_decimal(self):
-        self.assertEqual(quinary_to_decimal(20), 10)
-        self.assertEqual(quinary_to_decimal(0), 0)
-        self.assertEqual(quinary_to_decimal(4), 4)
-        self.assertEqual(quinary_to_decimal(100), 25)
+        self.assertEqual(self.calc.quinary_to_decimal(20), 10)
+        self.assertEqual(self.calc.quinary_to_decimal(0), 0)
+        self.assertEqual(self.calc.quinary_to_decimal(4), 4)
+        self.assertEqual(self.calc.quinary_to_decimal(100), 25)
 
 if __name__ == '__main__':
     unittest.main()
